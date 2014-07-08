8380a60ba8fa294c9284e02d9681ff33f071d315
Kiy Hong Tang <kiyhong@gmail.com>
2014-07-08 14:54:48 +0100

    Initial commit

---
+++ section-1.swift
@@ -0,0 +1,47 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+
+
+
+var cat = "🐱"; println(cat)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+
+
+let appleSummary = "I have \(apples) apples."
+let orangeSummary = "I have \(oranges) oranges"
+
+//
+//let explicitFloat: Float= 4.5
+//let float = 7.5
+//let floatCalulation = Float+\(float)
+
+let name = "kiy"
+let greeting = "Hello \(name), how are you today?"
+
+
+
+var shoppingList = ["catfish", "water", "tulips", "blue-paint"]
+shoppingList[1]
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "Public Relations"
+
+
+let emptyArray = String[]()
+let emptyDictionary = Dictionary<String, Float>()
+
+var stringArray:String[] = ["hello", "there","sir"]
